00bf69b6efa7c5a3759249c29d29e7e4ad028344
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-05-22 11:39:15 +0200

    update

--- utils/generator_uuid.py
+++
@@ -1,8 +0,0 @@
-import random
-import time
-
-def generate_id():
-    timestamp = int(time.time() * 1000)  # Millisecond precision
-    random_digits = random.randint(1000, 9999)  # 4 digits is sufficient for your volume
-    
-    return f"{timestamp}-{random_digits}"
